c389e7f8e2311c27eb8161a67484589828398458
Gabriel Trajano <gabriel-trajano@outlook.com>
2022-06-10 18:14:04 -0300

    Criado o teste para incluir um pet.

---
+++ src/test/java/petstore/pet.java
@@ -0,0 +1,37 @@
+package petstore;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+public class pet {
+    String uri = "https://petstore.swagger.io/v2/pet";
+
+    public String lerJSON(String caminhoJSON) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJSON)));
+    }
+
+    @Test
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJSON("db/pet1.json");
+
+        // Sintaxe Gherkin
+        // Dado - Quando - Então
+        // Given - When - Then
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri)
+        .then()
+                .log().all()
+                .statusCode(200);
+    }
+
+}
