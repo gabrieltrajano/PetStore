76e8386f3616c0c42409976222b82981e1b6e190
Gabriel Trajano <gabriel-trajano@outlook.com>
2022-06-11 13:12:18 -0300

    Completado os scripts alterarPet e excluirPet.

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,5 +1,6 @@
 package petstore;
 
+import org.apache.groovy.json.internal.IO;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -13,13 +14,13 @@ import static org.hamcrest.Matchers.contains;
 public class Pet {
     String uri = "https://petstore.swagger.io/v2/pet";
 
-    public String lerJSON(String caminhoJSON) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(caminhoJSON)));
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
 
-    @Test
+    @Test(priority = 1)
     public void incluirPet() throws IOException {
-        String jsonBody = lerJSON("db/pet1.json");
+        String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe Gherkin
         // Dado - Quando - Então
@@ -39,5 +40,56 @@ public class Pet {
                 .body("tags.name", contains("sta"));
 
     }
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petId = "19971230";
 
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Pluto"))
+                .body("status", is("available"))
+                .body("category.name", is("dog"));
+    }
+
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .put(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Augustus"))
+                .body("status", is("sold"));
+
+    }
+
+    @Test(priority = 4)
+    public void excluirPet() throws IOException {
+        String petId = "19971230";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is("19971230"));
+
+    }
 }
